3da048e51463af9633067fb0f50759bf9aa1c73d
finnaib <shoaibwwe01@gmail.com>
2023-03-31 17:07:29 +0530

    initial commit

---
+++ aosp_beryllium.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2018-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+
+# Inherit some common VoidUI stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# VoidUI Feature Flags
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+
+# VoidUI Maintainer Flags
+VOID_MAINTAINER := Finnaib
+CUSTOM_BUILD_TYPE := COMMUNITY
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_SYSTEM_NAME := beryllium
+
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
+    TARGET_PRODUCT="beryllium"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
